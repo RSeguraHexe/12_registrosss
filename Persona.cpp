12391d361bc42c0c67959f50db15f31e0aaaeace
RSeguraHexe <rsegurah@unjbg.edu.pe>
2026-06-24 11:25:18 -0500

    Personas, ejercicio

---
+++ Persona.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+struct Persona{
+    std::string dni;
+    float edad;
+};
+
+int main(){
+
+    struct Persona personas[100];
+    int n;
+
+    std::cout<<"=====================================================================\n";
+    std::cout<<"Ingrese la cantidad de personas: "; std::cin>>n;
+    std::cout<<"=====================================================================\n";
+
+    for(int i=0; i<n; i++){
+        std::cout<<"|"<<i+1<<"|\n";
+        std::cout<<"Ingrese el DNI de la persona: "; std::cin>>personas[i].dni;
+        std::cout<<"Ingrese la edad de la persona: "; std::cin>>personas[i].edad;
+        std::cout<<"=====================================================================\n";
+    }
+
+    int mayores50=0;
+    for(int i=0; i<n; i++){
+        if(personas[i].edad>50){
+            mayores50++;
+        }
+    }
+
+    int promedio, almacenar=0;
+    for(int i=0; i<n; i++){
+        almacenar=personas[i].edad+almacenar;
+    }
+
+    promedio=almacenar/n;
+
+    system("cls");
+    std::cout<<"=====================================================================\n";
+
+    std::cout<<"Cantidad de personas mayores a 50 anios: "<<mayores50<<"\n";
+    std::cout<<"Promedio de edad de las personas: "<<promedio<<"\n";
+    std::cout<<"=====================================================================\n";
+
+    for(int i=0; i<n; i++){
+        std::cout<<"DNI: "<<personas[i].dni<<" - Edad: "<<personas[i].edad<<"\n";
+    }
+    std::cout<<"=====================================================================\n";
+
+    return 0;
+}
